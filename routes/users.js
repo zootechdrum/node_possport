bedeedf98e6dd1fff27f3702ae8efbf7c68eb4be
Cesar Gomez <zootechdrum@gmail.com>
2020-03-15 08:45:19 -0700

    now hashing password

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+//User model
+
+const User = require('../models/User')
 
 //THis is the login page
 router.get('/login', (req, res) => res.render('login'))
@@ -35,7 +39,38 @@ router.post('/register', (req, res) => {
             password2
         })
     }else{
-        res.send('pass')
+        User.findOne({ email: email})
+        .then(user => {
+            //If user already exists
+            if(user){
+                errors.push({ msg: 'Email is already registered'});
+                res.render('register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                })
+            } else {
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                });
+                //Hash password
+                bcrypt.genSalt(10, (errors, salt) => bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) throw err;
+                    //hash the password from plain text to hash
+                    newUser.password = hash
+
+                    newUser.save()
+                    .then(user => {
+                        res.redirect('/users/login')
+                    })
+                    .catch(err => console.log(err))
+                }))
+            }
+        });
     }
 })
 
