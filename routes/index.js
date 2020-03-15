bf8066c92bd2756e7a2d0241d7823fbdb0a44f43
Cesar Gomez <zootechdrum@gmail.com>
2020-03-15 10:54:44 -0700

    provided authentication for dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const {ensureAuthenticated} = require('../config/auth')
 
 router.get('/', (req, res) => res.render('welcome'))
 
-router.get('/dashboard', (req, res) => res.render('dashboard'))
+router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard', {
+    name: req.user.name
+}))
 
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -88,10 +88,10 @@ router.post('/login', (req, res, next) => {
 });
 
 // Logout
-// router.get('/logout', (req, res) => {
-//   req.logout();
-//   req.flash('success_msg', 'You are logged out');
-//   res.redirect('/users/login');
-// });
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
+});
 
 module.exports = router;
