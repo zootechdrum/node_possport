0b536d885ea06e21926ca2b11e5052ba4eb42812
Cesar Gomez <zootechdrum@gmail.com>
2020-03-14 21:24:45 -0700

    added routes folder and using it in app

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express()
 
+//Routes
+app.use('/', require('./routes/index'));
+
 const PORT = process.env.PORT ||  3000;
 
 app.listen(PORT, console.log(`server started on port ${PORT} ` ))
